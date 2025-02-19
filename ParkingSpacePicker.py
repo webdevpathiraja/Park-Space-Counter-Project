45e2d120c85668ee6d473cb273d925a9de6435c3
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-02-19 19:10:31 +0530

    added interactive parking space selection with mouse clicks

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -5,16 +5,19 @@ img = cv2.imread('carParkImg.png')
 
 # width = 157-50, height = 240-192
 width, height = 107, 48
+positionList = []
+
+
+def mouseclick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        positionList.append((x, y))
+
 
 while True:
 
-    # drawing a rectangle on the image using OpenCV.
-    # img – The image on which the rectangle is drawn.
-    # The top-left corner of the rectangle (x=50, y=192).
-    # The bottom-right corner of the rectangle (x=157, y=240).
-    # The color of the rectangle in BGR format (Purple).
-    # The thickness of the rectangle's border (2 pixels).
-    # cv2.rectangle(img, (50, 192), (157, 240), (255, 0, 255), 2)
+    for position in positionList:
+        cv2.rectangle(img, position, (position[0] + width, position[1] + height), (255, 0, 255), 2)
 
     cv2.imshow("Car Park", img)
+    cv2.setMouseCallback("Car Park", mouseclick)
     cv2.waitKey(1)
