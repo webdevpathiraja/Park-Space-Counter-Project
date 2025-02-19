62ca6ba61ac40d2c8d2420a2bfe7bced7fc8fa83
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-02-19 19:19:00 +0530

    added right-click to remove a selected parking space

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -1,19 +1,27 @@
 import cv2
 import pickle
 
-img = cv2.imread('carParkImg.png')
-
 # width = 157-50, height = 240-192
 width, height = 107, 48
 positionList = []
 
 
 def mouseclick(events, x, y, flags, params):
+
+    # left-click to select a parking space
     if events == cv2.EVENT_LBUTTONDOWN:
         positionList.append((x, y))
 
+    # right-click to remove a selected parking space
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, position in enumerate(positionList):
+            x1, y1 = position
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                positionList.pop(i)
+
 
 while True:
+    img = cv2.imread('carParkImg.png')
 
     for position in positionList:
         cv2.rectangle(img, position, (position[0] + width, position[1] + height), (255, 0, 255), 2)
